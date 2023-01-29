0cccd405977dfd99ee32b1c15e18fbfe1fd23461
happy8131 <lion8131@naver.com>
2023-01-29 22:22:37 +0900

    change Status apply eslint

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -1,15 +1,29 @@
 import { Button, Form, Input } from "antd";
 import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import useInput from "../hooks/useInput";
+import { ADD_COMMENT_REQUEST } from "../reducers/post";
 
 const CommentForm = ({ post }) => {
   const id = useSelector((state) => state.user.me?.id);
-  const [commentText, onChangeCommentText] = useInput("");
+  const dispatch = useDispatch();
+  const { addCommentDone } = useSelector((state) => state.post);
+  const [commentText, onChangeCommentText, setCommentText] = useInput("");
+
+  useEffect(() => {
+    if (addCommentDone) {
+      setCommentText("");
+    }
+  }, [addCommentDone]);
 
   const onSubmitComment = useCallback(() => {
     console.log(post.id, commentText);
-  }, [commentText]);
+    dispatch({
+      type: ADD_COMMENT_REQUEST,
+      data: { content: commentText, postId: post.id, userId: id },
+    });
+  }, [commentText, id]);
 
   return (
     <Form onFinish={onSubmitComment}>
--- sagas/post.tsx
+++ sagas/post.tsx
@@ -1,5 +1,12 @@
 import axios from "axios";
 import { all, delay, fork, put, takeLatest } from "redux-saga/effects";
+import {
+  ADD_COMMENT_FAIL,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+} from "../reducers/post";
 
 function AddPostAPI(data) {
   return axios.post("/api/post", data);
@@ -8,14 +15,32 @@ function AddPostAPI(data) {
 function* addPost(action) {
   try {
     yield delay(1000);
-    yield put({ type: "ADD_POST_SUCCESS" });
+    yield put({ type: ADD_POST_SUCCESS });
   } catch (err) {}
 }
 
+function AddCommentAPI(data) {
+  return axios.post("/api/comment", data);
+}
+
+function* addComment(action) {
+  try {
+    yield delay(1000);
+    yield put({ type: ADD_COMMENT_SUCCESS });
+  } catch (err) {
+    yield delay(1000);
+    yield put({ type: ADD_COMMENT_FAIL });
+  }
+}
+
 function* watchAddPost() {
-  yield takeLatest("ADD_POST_REQUEST", addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
