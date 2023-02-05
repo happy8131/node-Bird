16bae0557b199454cd1302f461fd1c2c79f792e1
happy8131 <lion8131@naver.com>
2023-02-05 21:22:38 +0900

    faker i Dummy Data

--- components/PostCardContent.tsx
+++ components/PostCardContent.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 
 const PostCardContent = ({ postData }) => {
+  console.log(postData);
   return (
     <div>
-      {postData.split(/(#[^\s#]+)/g).map((v, i) => {
-        if (v.match(/(#[^\s#]+)/)) {
-          return (
-            <Link key={i} href={`/hashtag/${v.slice(1)}`}>
-              {v}
-            </Link>
-          );
-        }
-        return v;
-      })}
+      {postData
+        .toString()
+        .split(/(#[^\s#]+)/g)
+        .map((v, i) => {
+          if (v.match(/(#[^\s#]+)/)) {
+            return (
+              <Link key={i} href={`/hashtag/${v.slice(1)}`}>
+                {v}
+              </Link>
+            );
+          }
+          return v;
+        })}
     </div>
   );
 };
