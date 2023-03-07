44c283f1b79f92d96e016668303ccc2af2b176a1
happy8131 <lion8131@naver.com>
2023-03-08 00:29:23 +0900

    image Upload for multer

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -2,7 +2,7 @@ import { Button, Form, Input } from "antd";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useInput from "../hooks/useInput";
-import { addPost } from "../reducers/post";
+import { addPost, UPLOAD_IMAGES_REQUEST } from "../reducers/post";
 
 const PostForm = () => {
   const { imagePaths, addPostDone } = useSelector((state) => state.post);
@@ -25,6 +25,19 @@ const PostForm = () => {
     imageInput.current.click();
   }, [imageInput.current]);
 
+  const onChangeImages = useCallback((e) => {
+    console.log("images", e.target.files);
+    const imageFormData = new FormData();
+
+    [].forEach.call(e.target.files, (f) => {
+      imageFormData.append("image", f);
+    });
+    dispatch({
+      type: UPLOAD_IMAGES_REQUEST,
+      data: imageFormData,
+    });
+  }, []);
+  console.log(imagePaths);
   return (
     <Form
       style={{ margin: "10px 0 20px" }}
@@ -38,7 +51,14 @@ const PostForm = () => {
         placeholder="어떤 신기한 일이 있었나요?"
       />
       <div>
-        <input type="file" multiple hidden ref={imageInput} />
+        <input
+          type="file"
+          name="image"
+          multiple
+          hidden
+          ref={imageInput}
+          onChange={onChangeImages}
+        />
         <Button onClick={onClickImageUpload}>이미지 업로드</Button>
         <Button type="primary" style={{ float: "right" }} htmlType="submit">
           짹짹
@@ -47,7 +67,11 @@ const PostForm = () => {
       <div>
         {imagePaths?.map((v, idx) => (
           <div key={idx} style={{ display: "inline-block" }}>
-            <img src={v} style={{ width: "200px" }} alt={v} />
+            <img
+              src={v.replace(/\/thumb\//, "/original/")}
+              style={{ width: "200px" }}
+              alt={v}
+            />
             <div>
               <Button>제거</Button>
             </div>
